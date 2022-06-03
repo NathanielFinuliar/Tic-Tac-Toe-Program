a4c1b1d4f31f5e4fe8ec150a3043919ce1e669f3
NathanielFinuliar <nathaniel.finuliar14@gmail.com>
2022-06-04 01:54:21 +0800

    Initial Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace Tic_Tac_Toe_Program
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Bttn1 = new System.Windows.Forms.Button();
+            this.Bttn2 = new System.Windows.Forms.Button();
+            this.Bttn3 = new System.Windows.Forms.Button();
+            this.Bttn4 = new System.Windows.Forms.Button();
+            this.Bttn5 = new System.Windows.Forms.Button();
+            this.Bttn6 = new System.Windows.Forms.Button();
+            this.Bttn7 = new System.Windows.Forms.Button();
+            this.Bttn8 = new System.Windows.Forms.Button();
+            this.Bttn9 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Bttn1
+            // 
+            this.Bttn1.Location = new System.Drawing.Point(192, 125);
+            this.Bttn1.Name = "Bttn1";
+            this.Bttn1.Size = new System.Drawing.Size(125, 100);
+            this.Bttn1.TabIndex = 0;
+            this.Bttn1.UseVisualStyleBackColor = true;
+            // 
+            // Bttn2
+            // 
+            this.Bttn2.Location = new System.Drawing.Point(192, 231);
+            this.Bttn2.Name = "Bttn2";
+            this.Bttn2.Size = new System.Drawing.Size(125, 100);
+            this.Bttn2.TabIndex = 0;
+            this.Bttn2.UseVisualStyleBackColor = true;
+            // 
+            // Bttn3
+            // 
+            this.Bttn3.Location = new System.Drawing.Point(192, 337);
+            this.Bttn3.Name = "Bttn3";
+            this.Bttn3.Size = new System.Drawing.Size(125, 100);
+            this.Bttn3.TabIndex = 0;
+            this.Bttn3.UseVisualStyleBackColor = true;
+            // 
+            // Bttn4
+            // 
+            this.Bttn4.Location = new System.Drawing.Point(342, 125);
+            this.Bttn4.Name = "Bttn4";
+            this.Bttn4.Size = new System.Drawing.Size(125, 100);
+            this.Bttn4.TabIndex = 0;
+            this.Bttn4.UseVisualStyleBackColor = true;
+            // 
+            // Bttn5
+            // 
+            this.Bttn5.Location = new System.Drawing.Point(342, 231);
+            this.Bttn5.Name = "Bttn5";
+            this.Bttn5.Size = new System.Drawing.Size(125, 100);
+            this.Bttn5.TabIndex = 0;
+            this.Bttn5.UseVisualStyleBackColor = true;
+            // 
+            // Bttn6
+            // 
+            this.Bttn6.Location = new System.Drawing.Point(342, 337);
+            this.Bttn6.Name = "Bttn6";
+            this.Bttn6.Size = new System.Drawing.Size(125, 100);
+            this.Bttn6.TabIndex = 0;
+            this.Bttn6.UseVisualStyleBackColor = true;
+            // 
+            // Bttn7
+            // 
+            this.Bttn7.Location = new System.Drawing.Point(490, 125);
+            this.Bttn7.Name = "Bttn7";
+            this.Bttn7.Size = new System.Drawing.Size(125, 100);
+            this.Bttn7.TabIndex = 0;
+            this.Bttn7.UseVisualStyleBackColor = true;
+            // 
+            // Bttn8
+            // 
+            this.Bttn8.Location = new System.Drawing.Point(490, 231);
+            this.Bttn8.Name = "Bttn8";
+            this.Bttn8.Size = new System.Drawing.Size(125, 100);
+            this.Bttn8.TabIndex = 0;
+            this.Bttn8.UseVisualStyleBackColor = true;
+            // 
+            // Bttn9
+            // 
+            this.Bttn9.Location = new System.Drawing.Point(490, 337);
+            this.Bttn9.Name = "Bttn9";
+            this.Bttn9.Size = new System.Drawing.Size(125, 100);
+            this.Bttn9.TabIndex = 0;
+            this.Bttn9.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(869, 550);
+            this.Controls.Add(this.Bttn9);
+            this.Controls.Add(this.Bttn6);
+            this.Controls.Add(this.Bttn3);
+            this.Controls.Add(this.Bttn8);
+            this.Controls.Add(this.Bttn5);
+            this.Controls.Add(this.Bttn2);
+            this.Controls.Add(this.Bttn7);
+            this.Controls.Add(this.Bttn4);
+            this.Controls.Add(this.Bttn1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Bttn1;
+        private System.Windows.Forms.Button Bttn2;
+        private System.Windows.Forms.Button Bttn3;
+        private System.Windows.Forms.Button Bttn4;
+        private System.Windows.Forms.Button Bttn5;
+        private System.Windows.Forms.Button Bttn6;
+        private System.Windows.Forms.Button Bttn7;
+        private System.Windows.Forms.Button Bttn8;
+        private System.Windows.Forms.Button Bttn9;
+    }
+}
+
