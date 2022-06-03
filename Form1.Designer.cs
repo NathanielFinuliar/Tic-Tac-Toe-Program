a0420790eadf1f82861f67a67465c3126786999d
NathanielFinuliar <nathaniel.finuliar14@gmail.com>
2022-06-04 03:16:26 +0800

    Reset and Exit button are now added

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -39,77 +39,79 @@
             this.Bttn8 = new System.Windows.Forms.Button();
             this.Bttn9 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Reset = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // Bttn1
             // 
-            this.Bttn1.Location = new System.Drawing.Point(211, 125);
+            this.Bttn1.Location = new System.Drawing.Point(226, 142);
             this.Bttn1.Name = "Bttn1";
-            this.Bttn1.Size = new System.Drawing.Size(125, 100);
+            this.Bttn1.Size = new System.Drawing.Size(104, 91);
             this.Bttn1.TabIndex = 0;
             this.Bttn1.UseVisualStyleBackColor = true;
             // 
             // Bttn2
             // 
-            this.Bttn2.Location = new System.Drawing.Point(211, 231);
+            this.Bttn2.Location = new System.Drawing.Point(226, 248);
             this.Bttn2.Name = "Bttn2";
-            this.Bttn2.Size = new System.Drawing.Size(125, 100);
+            this.Bttn2.Size = new System.Drawing.Size(104, 91);
             this.Bttn2.TabIndex = 0;
             this.Bttn2.UseVisualStyleBackColor = true;
             // 
             // Bttn3
             // 
-            this.Bttn3.Location = new System.Drawing.Point(211, 337);
+            this.Bttn3.Location = new System.Drawing.Point(226, 354);
             this.Bttn3.Name = "Bttn3";
-            this.Bttn3.Size = new System.Drawing.Size(125, 100);
+            this.Bttn3.Size = new System.Drawing.Size(104, 91);
             this.Bttn3.TabIndex = 0;
             this.Bttn3.UseVisualStyleBackColor = true;
             // 
             // Bttn4
             // 
-            this.Bttn4.Location = new System.Drawing.Point(342, 125);
+            this.Bttn4.Location = new System.Drawing.Point(357, 142);
             this.Bttn4.Name = "Bttn4";
-            this.Bttn4.Size = new System.Drawing.Size(125, 100);
+            this.Bttn4.Size = new System.Drawing.Size(104, 91);
             this.Bttn4.TabIndex = 0;
             this.Bttn4.UseVisualStyleBackColor = true;
             // 
             // Bttn5
             // 
-            this.Bttn5.Location = new System.Drawing.Point(342, 231);
+            this.Bttn5.Location = new System.Drawing.Point(357, 248);
             this.Bttn5.Name = "Bttn5";
-            this.Bttn5.Size = new System.Drawing.Size(125, 100);
+            this.Bttn5.Size = new System.Drawing.Size(104, 91);
             this.Bttn5.TabIndex = 0;
             this.Bttn5.UseVisualStyleBackColor = true;
             // 
             // Bttn6
             // 
-            this.Bttn6.Location = new System.Drawing.Point(342, 337);
+            this.Bttn6.Location = new System.Drawing.Point(357, 354);
             this.Bttn6.Name = "Bttn6";
-            this.Bttn6.Size = new System.Drawing.Size(125, 100);
+            this.Bttn6.Size = new System.Drawing.Size(104, 91);
             this.Bttn6.TabIndex = 0;
             this.Bttn6.UseVisualStyleBackColor = true;
             // 
             // Bttn7
             // 
-            this.Bttn7.Location = new System.Drawing.Point(473, 125);
+            this.Bttn7.Location = new System.Drawing.Point(488, 142);
             this.Bttn7.Name = "Bttn7";
-            this.Bttn7.Size = new System.Drawing.Size(125, 100);
+            this.Bttn7.Size = new System.Drawing.Size(104, 91);
             this.Bttn7.TabIndex = 0;
             this.Bttn7.UseVisualStyleBackColor = true;
             // 
             // Bttn8
             // 
-            this.Bttn8.Location = new System.Drawing.Point(473, 231);
+            this.Bttn8.Location = new System.Drawing.Point(488, 248);
             this.Bttn8.Name = "Bttn8";
-            this.Bttn8.Size = new System.Drawing.Size(125, 100);
+            this.Bttn8.Size = new System.Drawing.Size(104, 91);
             this.Bttn8.TabIndex = 0;
             this.Bttn8.UseVisualStyleBackColor = true;
             // 
             // Bttn9
             // 
-            this.Bttn9.Location = new System.Drawing.Point(473, 337);
+            this.Bttn9.Location = new System.Drawing.Point(488, 354);
             this.Bttn9.Name = "Bttn9";
-            this.Bttn9.Size = new System.Drawing.Size(125, 100);
+            this.Bttn9.Size = new System.Drawing.Size(104, 91);
             this.Bttn9.TabIndex = 0;
             this.Bttn9.UseVisualStyleBackColor = true;
             // 
@@ -120,12 +122,30 @@
             this.textBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
             this.textBox1.Font = new System.Drawing.Font("Games", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox1.ForeColor = System.Drawing.Color.DeepPink;
-            this.textBox1.Location = new System.Drawing.Point(217, 46);
+            this.textBox1.Location = new System.Drawing.Point(213, 63);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(374, 73);
+            this.textBox1.Size = new System.Drawing.Size(379, 73);
             this.textBox1.TabIndex = 1;
             this.textBox1.Text = "Tic Tac Toe";
             this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(721, 444);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(113, 66);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Exit";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Reset
+            // 
+            this.Reset.Location = new System.Drawing.Point(721, 366);
+            this.Reset.Name = "Reset";
+            this.Reset.Size = new System.Drawing.Size(113, 66);
+            this.Reset.TabIndex = 3;
+            this.Reset.Text = "Reset";
+            this.Reset.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
@@ -134,6 +154,8 @@
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(869, 550);
+            this.Controls.Add(this.Reset);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.Bttn9);
             this.Controls.Add(this.Bttn6);
@@ -164,6 +186,8 @@
         private System.Windows.Forms.Button Bttn8;
         private System.Windows.Forms.Button Bttn9;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button Reset;
     }
 }
 
